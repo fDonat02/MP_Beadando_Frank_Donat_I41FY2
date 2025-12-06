071e82e0cc6d8dec5ee4166119ab58efc00548fc
GangstaPants <102move@gmail.com>
2025-12-06 15:47:38 +0100

    FridgeItem class

---
+++ Fridge_Shopping_app/Fridge_Shopping_app/FridgeItem.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fridge_Shopping_app
+{
+    internal partial class FridgeItem : ObservableObject
+    {
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        float quantity;
+
+        [ObservableProperty]
+        DateOnly exp_date;
+    }
+}
