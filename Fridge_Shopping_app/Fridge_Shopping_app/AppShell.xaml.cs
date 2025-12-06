1b1edf15d16b63086459a51a858326d10b87137b
GangstaPants <102move@gmail.com>
2025-12-06 15:28:58 +0100

    Project creation

---
+++ Fridge_Shopping_app/Fridge_Shopping_app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Fridge_Shopping_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Fridge_Shopping_app/Fridge_Shopping_app/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Fridge_Shopping_app")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Fridge_Shopping_app.Pages")]
